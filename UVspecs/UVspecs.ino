353196220a0ffc5c187731172967c943c1c9189a
Clement <clement.caloin@etu.emse.fr>
2019-01-21 15:19:07 +0100

    Motor code
    Adding the code to pilot the motor to maximise the amount of energy produced by the solar panel.

--- UVspecs/UVspecs.ino
+++ UVspecs/UVspecs.ino
@@ -1,25 +1,56 @@
+float voltageA0, voltageA1, voltageA2;
+float valA0, valA1, valA2;
+
+int voltageT1, voltageT2;
+
+
+int pin1 = 7;
+int pin2 = 8;
+int pinPWM = 5;
+float maxVoltage = 1.68;
+float voltageValues[] = {0.8, 0.8, 0.8}; 
+int numberOfValues = 3;
+bool sens = true;
+int state1 = 1;
+int state2 = 0;
+float goalValue = 0;
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  
+  pinMode(pinPWM, OUTPUT); // Pin PWM
+  pinMode(pin1, OUTPUT); // Pin commande 1
+  pinMode(pin2, OUTPUT); // Pin commande 2
 
 }
+
+
 
 void loop() {
   // put your main code here, to run repeatedly:
   analogWrite(3,255);
   
-  float valA1 = analogRead(A1);
-  float voltageA1 = valA1*5/1024;
+  valA1 = analogRead(A1);
+  voltageA1 = valA1*5/1024;
   
-  float valA0 = analogRead(A0);
-  float voltageA0 = valA0*5/1024;
+  valA0 = analogRead(A0);
+  voltageA0 = valA0*5/1024;
   
   float current = voltageA1/10;
 
-  float valA2 = analogRead(A2);
-  float voltageA2 = valA2*5/1024;
+  valA2 = analogRead(A2);
+  voltageA2 = valA2*5/1024;
+
+   if(voltageA2 < averageVoltageValue()){
+    goalValue = averageVoltageValue();
+    lookingForMPPT();
+  }
+  else{
+    //Do nothing, the solar panel is fine
+  }
+
+  updateVoltageValues(voltageA2);
   
   Serial.write("Voltage A0: ");
   Serial.print(voltageA0); 
@@ -36,6 +67,77 @@ void loop() {
   Serial.println();
   Serial.println();
 
+ 
+
+  analogWrite(pinPWM, 1);
+  digitalWrite(pin1, 1);
+  digitalWrite(pin2, 0);
+
   
   delay(1000);
+}
+
+float readA2(){
+  valA2 = analogRead(A2);
+  voltageA2 = valA2*5/1024;
+  return voltageA2;
+}
+
+void updateVoltageValues(float newVoltage){
+  for(int i = 0; i< numberOfValues - 1; i++){
+    voltageValues[i] = voltageValues[i+1];
+  }
+  voltageValues[numberOfValues] = newVoltage;
+  
+}
+
+float averageVoltageValue(){
+  return ((voltageValues[0] + voltageValues[1] + voltageValues[2]) / 3);
+}
+
+void actionMotor(int currentSens){
+  if(currentSens){
+    state1 = 1;
+    state2 = 0;
+  }
+  else{
+    state1 = 0;
+    state2 = 1;
+  }
+
+  
+  analogWrite(pinPWM, 255);
+  digitalWrite(pin1, state1);
+  digitalWrite(pin2, state2);
+  delay(1000);
+}
+
+void stopMotor(){
+  analogWrite(pinPWM, 0);
+}
+
+void readAndUpdateA2(){
+  voltageT1 = voltageT2;
+  voltageT2 = readA2();
+}
+
+void lookingForMPPT(){
+  voltageT1 = readA2();
+  actionMotor(sens);
+  voltageT2 = readA2();
+
+  while( voltageT2 > voltageT1 ){
+    actionMotor(sens);
+    readAndUpdateA2();
+  }
+
+  sens = !sens;
+
+  while( voltageT2 > voltageT1 ){
+    actionMotor(sens);
+    readAndUpdateA2();
+  }
+
+  stopMotor();
+  
 }
