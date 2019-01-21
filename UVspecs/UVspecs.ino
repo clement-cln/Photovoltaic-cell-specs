dd2a0c3786177f66b12a134cc6c489ffb03abfd9
Clement <clement.caloin@etu.emse.fr>
2019-01-21 11:50:49 +0100

    Change of values
    Changing the values of the resistors, Adding a scope to verify that the load is working.

--- UVspecs/UVspecs.ino
+++ UVspecs/UVspecs.ino
@@ -8,24 +8,34 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  analogWrite(3,255);
+  
   float valA1 = analogRead(A1);
-  float voltageA1 = valA1*5/1.024;
+  float voltageA1 = valA1*5/1024;
+  
   float valA0 = analogRead(A0);
   float voltageA0 = valA0*5/1024;
-  float current = voltageA1/24;
+  
+  float current = voltageA1/10;
+
+  float valA2 = analogRead(A2);
+  float voltageA2 = valA2*5/1024;
+  
   Serial.write("Voltage A0: ");
   Serial.print(voltageA0); 
-  Serial.write(" V");
-
-  Serial.write(" Voltage A1: ");
-  Serial.print(voltageA1); 
   Serial.write(" V");
  
   Serial.write(", Current: ");
   Serial.print(current);
   Serial.write(" mA");
+
+  Serial.write(" Voltage A2: ");
+  Serial.print(voltageA2); 
+  Serial.write(" V");
+  
   Serial.println();
   Serial.println();
+
   
   delay(1000);
 }
